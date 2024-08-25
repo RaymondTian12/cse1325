55f012dcc55e025854f8c49d7a9ea8aefa4332c2
Raymond Tian <rxt9181@mavs.uta.edu>
2024-08-25 14:12:05 -0500

    P01 extreme bonus third draft

--- P01/extreme_bonus/Hello.java
+++ P01/extreme_bonus/Hello.java
@@ -1,9 +1,8 @@
-
 public class Hello
 {
 	public static void main(String[] args)
 	{
-		String userName = System.getProperty("user.name");
+		String userName = System.getProperty("user.dir");
 		System.out.println("hello, " + userName);
 	}
 }
