805a1ccd1ff7ccdab03303a543a57661a3e2398d
Raymond Tian <rxt9181@mavs.uta.edu>
2024-08-31 23:26:11 -0500

    Denomination.java 2nd draft

--- P02/bonus/Denomination.java
+++ P02/bonus/Denomination.java
@@ -1,4 +1,24 @@
 public enum Denomination
 {
+	PENNY(0.01),
+	NICKEL(0.05),
+	DIME(0.10),
+	QUARTER(0.25);
 	
+	private double value;
+	private Denomination(double value)
+	{
+		this.value = value;
+	}	
+	
+	public double getValue()
+	{
+		return value;
+	}
+	
+	@Override
+	public string toString()
+	{
+		return name().toLowercase();
+	}
 }
