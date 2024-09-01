cbb6d16de69832b65c16bc21c3b374913217fdac
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-01 00:35:34 -0500

    Purse.java 2nd draft

--- P02/bonus/Purse.java
+++ P02/bonus/Purse.java
@@ -27,7 +27,10 @@ public class Purse
 			}
 
 		}
-	
+		
+		System.out.println("The total value of your purse is: $" + totalValue);
+		System.out.println("Earliest Year: " + earliestDate);
+		System.out.print("Latest Year: "+ latestDate);
 	}
 
 }
