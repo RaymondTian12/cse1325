02a379f0c2292515c10c5ccfa3e8d842c5f4cc41
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-01 19:36:58 -0500

    Fixed compiler errors in Denomination.java

--- P02/extreme_bonus/Denomination.java
+++ P02/extreme_bonus/Denomination.java
@@ -1,9 +1,9 @@
 public enum Denomination
 {
-	PENNY,
-	NICKEL,
-	DIME,
-	QUARTER;
+	PENNY(0.01),
+	NICKEL(0.05),
+	DIME(0.10),
+	QUARTER(0.25);
 	
 	private double value;
 	private Denomination(double value)
