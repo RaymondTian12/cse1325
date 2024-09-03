17e2fd27926911b70bb598b207bfe89c91cffd57
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-03 07:39:26 -0500

    Fixed getWeight() method

--- P02/extreme_bonus/Coin.java
+++ P02/extreme_bonus/Coin.java
@@ -20,7 +20,7 @@ public class Coin
 		{
 			case PENNY -> {return (year < 1983) ? 3.110 : 2.500;}
 			case NICKEL -> {return 5.000;}
-			case DIME -> {return (year < 1983) ? 2.500 : 2.268;}
+			case DIME -> {return (year < 1965) ? 2.500 : 2.268;}
 			case QUARTER -> {return (year < 1965) ? 6.250 : 5.670;}
 			default -> {throw new IllegalArgumentException("Invalid denomination: " + denomination);}
 		}
