8274a8644bfd33ea6fe3e71944f8664dc149ad13
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-08 11:31:39 -0500

    Student.java 1st draft

---
+++ P03/Student.java
@@ -0,0 +1,25 @@
+public class Student
+{
+	private String name;
+	private int id;
+	private String email;
+	private Account account;
+	
+	public student(String name, int id, String, email)
+	{
+		if (!email.endsWith("@uta.edu") && !email.endsWith("@mavs.uta.edu")
+		{
+			throw new IllegalArgumentException("Non-UTA email addres: " + email);
+		}
+		
+		this.name = name;
+		this.id = id;
+		this.email = email;
+		this.account = account;
+	}
+	
+	public String requestMedia(Media media)
+	{
+		return account.play(media);
+	}
+}
