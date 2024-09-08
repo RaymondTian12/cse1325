8546c343f126b21c07fefdf049d65a96194d9899
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-08 09:27:26 -0500

    TestMedia.java added test data and error handling

--- P03/TestMedia.java
+++ P03/TestMedia.java
@@ -2,6 +2,12 @@ public class TestMedia
 {
 	public static void main(String[] args)
 	{
+		Media media = new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0"); // Hardcode test data : Title, URL
 		
+		if (!(media.toString()).equals("The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0)")
+		{
+			System.err.println("FAIL: " + "Expected String - \"The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0)\" "  + "Actual String - \ " + media.toString();
+			System.exit(-1);
+		}
 	}
 }
