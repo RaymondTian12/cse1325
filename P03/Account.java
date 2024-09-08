d379fdd45de0f81de87a9dcee61df60729200986
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-08 01:24:57 -0500

    Account.java added getAccountNumber and started play method

--- P03/Account.java
+++ P03/Account.java
@@ -1,10 +1,20 @@
 public class Account
 {
-	private static int accountNumber = 1; // Static and starts with 1
-	private int nextAccountNumber;
+	private int accountNumber; 
+	private static int nextAccountNumber = 1; // Static and starts with 1
 	
 	public Account()
 	{
-		
+		this.accountNumber = acc
+	}
+	
+	public int getAccountNumber()
+	{
+		return accountNumber;
+	}
+	
+	public String play(Media media)
+	{
+		return "Playing" + 
 	}
 }
