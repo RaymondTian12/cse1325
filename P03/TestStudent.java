da662ec815309a3667e5b61529c310c4ac4dd128
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-08 16:32:33 -0500

    TestStudent.java testing using account

--- P03/TestStudent.java
+++ P03/TestStudent.java
@@ -5,15 +5,14 @@ public class TestStudent
 		int failureCount = 0;
 		
 		Student student = new Student("Raymond Tian", 1002199181, "rxt9181@mavs.uta.edu"); // Test data
+		Account account = new Account(); // Test data
 		
-		System.out.println(student.toString());
-		
-		/*if (!student.toString().equals("Raymond Tian (1002199181, rxt9181@mavsuta.edu, Account #" + student.account.getAccountNumber() + ")"));
+		if (!student.toString().equals("Raymond Tian (1002199181, rxt9181@mavsuta.edu, Account #" + account.getAccountNumber() + ")"));
 		{
-			
+			System.err.println("FAIL: ");
 			failureCount++;
 		}
-		*/
+		
 		
 		System.exit(failureCount);
 	}
