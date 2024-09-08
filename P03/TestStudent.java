71719bf0699060cbb9982eb085bec3d404854841
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-08 16:18:00 -0500

    TestStudent.java added test for toString() method

--- P03/TestStudent.java
+++ P03/TestStudent.java
@@ -3,5 +3,16 @@ public class TestStudent
 	public static void main(String[] args)
 	{
 		int failureCount = 0;
+		
+		Student student = new Student("Raymond Tian", 1002199181, "rxt9181@mavs.uta.edu"); // Test data
+		
+		if (!student.toString().equals("Raymond Tian (1002199181, rxt9181@mavsuta.edu, Account #" + student.account.getAccountNumber() + ")"));
+		{
+			
+			failureCount++;
+		}
+		
+		
+		System.exit(failureCount);
 	}
 }
