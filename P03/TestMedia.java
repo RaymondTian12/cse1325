053ffc3338a53d1585eff1cf0657e1136fdff840
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-08 09:42:11 -0500

    TestMedia.java fixed compiler errors

--- P03/TestMedia.java
+++ P03/TestMedia.java
@@ -4,9 +4,9 @@ public class TestMedia
 	{
 		Media media = new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0"); // Hardcode test data : Title, URL
 		
-		if (!(media.toString()).equals("The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0)")
+		if (!(media.toString()).equals("The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0)"))
 		{
-			System.err.println("FAIL: " + "Expected String - \"The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0)\" "  + "Actual String - \ " + media.toString();
+			System.err.println("FAIL: " + "Expected String - \"The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0)\" "  + "Actual String -  " + media.toString());
 			System.exit(-1);
 		}
 	}
