d70951e2ed453d79a9761bf4476293309d1d2d25
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-09 16:58:28 -0500

    TestAccount.java adjusted output formatting

--- P03/TestAccount.java
+++ P03/TestAccount.java
@@ -4,16 +4,18 @@ public class TestAccount
 	{
 		Account account1 = new Account(); // Should be 1
 		Account account2 = new Account(); // Should be 2
+		int account1Expected = 1;
+		int account2Expected = 2;
 		
-		if (account1.getAccountNumber() != 1)
+		if (account1.getAccountNumber() != account1Expected)
 		{
-			System.err.println("FAIL: Expected Account Number - 1 " + "Actual Account Number - " + account1.getAccountNumber());
+			System.err.println("FAIL: Expected Account Number - " + account1Expected + " | Actual Account Number - " + account1.getAccountNumber());
 			System.exit(-1);
 		}
 		
-		if (account2.getAccountNumber() != 2)
+		if (account2.getAccountNumber() != account2Expected)
 		{
-			System.err.println("FAIL: Expected Account Number - 2 " + "Actual Account Number - " + account2.getAccountNumber());
+			System.err.println("FAIL: Expected Account Number - " + account2Expected + " | Actual Account Number - " + account2.getAccountNumber());
 			System.exit(-1);
 		}
 	}
