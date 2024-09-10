4e3520f2a5fc92eb85c36318e2b5266d43336e7c
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-09 21:15:30 -0500

    TestMedia.java implemented testing for valid/invalid URLs

--- P03/TestMedia.java
+++ P03/TestMedia.java
@@ -9,5 +9,37 @@ public class TestMedia
 			System.err.println("FAIL: " + "Expected String - \"The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0)\" "  + "Actual String -  " + media.toString());
 			System.exit(-1);
 		}
+		
+		String[] validURLs = {"https://youtube.com", "file://media/lib/garp.mp4"};
+		String[] invalidURLs = {"hello.world", "htt://badurl.com", "flub://badurl.com"};
+		
+		for (String url : validURLs)
+		{
+			try
+			{
+				new Media("Default Title for validURL", url);
+				System.out.println("PASS: " + url + " is valid");
+			}
+			catch(RuntimeException e)
+			{
+				System.err.println("FAIL: Unexpected exception for " + url);
+				System.err.println(e);
+			}
+		}
+		
+		for (String url : invalidURLs)
+		{
+			try
+			{
+				new Media("Default Title for invalidURL", url);
+				System.out.print("FAIL: Missing exception for " + url);
+			}
+			catch(RuntimeException e)
+			{
+				System.err.println("Invalid URL " + url + " successfully detected");
+				System.err.println(e);
+			}
+		}
+		
 	}
 }
