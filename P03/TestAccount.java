7a1bb5677ae09ef2e9cd0e5916a6cd2f65de0c82
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-10 00:08:38 -0500

    TestAccont.java modified output formatting to better display error messages/code

--- P03/TestAccount.java
+++ P03/TestAccount.java
@@ -2,6 +2,8 @@ public class TestAccount
 {
 	public static void main(String[] args)
 	{
+		int failureCount = 0;
+		
 		Account account1 = new Account(); // Should be 1
 		Account account2 = new Account(); // Should be 2
 		int account1Expected = 1;
@@ -10,13 +12,23 @@ public class TestAccount
 		if (account1.getAccountNumber() != account1Expected)
 		{
 			System.err.println("FAIL: Expected Account Number - " + account1Expected + " | Actual Account Number - " + account1.getAccountNumber());
-			System.exit(-1);
+			failureCount++;
 		}
 		
 		if (account2.getAccountNumber() != account2Expected)
 		{
 			System.err.println("FAIL: Expected Account Number - " + account2Expected + " | Actual Account Number - " + account2.getAccountNumber());
-			System.exit(-1);
+			failureCount++;
+		}
+		
+		if (failureCount > 0)
+		{
+			System.err.println("FAIL: Error code " + failureCount);
+			System.exit(failureCount);
+		}
+		else
+		{
+			System.exit(0);
 		}
 	}
 }
