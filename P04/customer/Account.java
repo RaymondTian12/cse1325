1c189fccf868e100f440b1bd03516ff6be16aa0c
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-15 18:55:30 -0500

    Added JavaDoc for Account.java

--- P04/customer/Account.java
+++ P04/customer/Account.java
@@ -1,3 +1,11 @@
+/**
+ * Represents an account management system
+ *
+ * @author	Raymond Tian
+ * @version	1.0
+ * @since	1.0
+ */
+
 package customer;
 
 import product.Media;
@@ -7,16 +15,33 @@ public abstract class Account
 	private int accountNumber; 
 	private static int nextAccountNumber = 1; // Static and starts with 1
 	
+	/**
+	 * Constructs a new account with a unique associated account number
+	 * 
+	 * @since	1.0
+	 */
 	public Account()
 	{
 		this.accountNumber = nextAccountNumber++;
 	}
 	
+	/**
+	 * Returns an account's associated account number
+	 * 
+	 * @return	the account's associated account number
+	 * @since	1.0
+	 */
 	public int getAccountNumber()
 	{
 		return accountNumber;
 	}
 	
-	
+	/**
+	 * An abstract method for playing media. This will be implemented depending on the subclass account type
+	 * 
+	 * @param media		the media to be played
+	 * @return		a string with a message indicating that the media will be played or not
+	 * @since		1.0
+	 */
 	public abstract String play(Media media);
 }
