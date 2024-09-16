7d86660c7235ad71f4fd5a7fe26c199af51cca94
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-15 19:17:46 -0500

    Added JavaDoc for Alacarte.java

--- P04/customer/Alacarte.java
+++ P04/customer/Alacarte.java
@@ -1,3 +1,11 @@
+/**
+ * Rerepresents an alacarte account type with point restrictions to playing media
+ *
+ * @author	Raymond Tian
+ * @version	1.0
+ * @since	1.0
+ */
+
 package customer;
 
 import product.Media;
@@ -6,16 +14,34 @@ public class Alacarte extends Account
 {
 	private int pointsRemaining;
 	
+	/**
+	 * Adds the number of points to the points remaining balance
+	 *
+	 * @param points	the number of points to be added
+	 * @since		1.0
+	 */
 	public void buyPoints(int points)
 	{
 		pointsRemaining += points;
 	}
 	
+	/**
+	 * Returns the number of points remaining in the account 
+	 * 
+	 * @return	the number of points remaining in the account
+	 */
 	public int getPointsRemaining()
 	{
 		return pointsRemaining;
 	}
 	
+	/**
+	 * Returns a message displaying the media to be played depending on the student's points
+	 * 
+	 * @param media		the media to be played
+	 * @return		a string displaying the media to be played and the points required if the student doesn't have enough
+	 * @since 		1.0
+	 */
 	@Override
 	public String play(Media media)
 	{
