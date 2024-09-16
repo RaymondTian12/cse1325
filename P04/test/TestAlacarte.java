84ea7abad8ce463076439d74fa1c5c3c2502ef24
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-16 14:57:07 -0500

    Finished each regression test and compiled with no warnings/error - not tested yet

--- P04/test/TestAlacarte.java
+++ P04/test/TestAlacarte.java
@@ -7,9 +7,10 @@ public class TestAlacarte
 {
 	public static void main(String[] args)
 	{
-		Media testMedia = new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0", 4);
+		int testMediaPoints = 4; // Adjust to check what happens if account has enough or not enough points
+		Media testMedia = new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0", testMediaPoints);
 		int pointsBought = 5;
-		int failureCount = 0;
+		int failureCount = 0; // Used to count the number of failures and exit the program
 		
 		// Verify that buying points inceases the points remaining by the number of points bought
 		Alacarte account1 = new Alacarte();
@@ -25,10 +26,10 @@ public class TestAlacarte
 		
 		// Verify that play returns "Playing " and the media if sufficient points are available 
 		String expected1 = "Playing " + testMedia.toString();
-		if (!expected.equals(account1.play(testMedia)))
+		if (!expected1.equals(account1.play(testMedia)))
 		{
 			System.err.println("FAIL: Incorrect play message displayed");
-			System.err.println("Expected: " + expected);
+			System.err.println("Expected: " + expected1);
 			System.err.println("Actual: " + account1.play(testMedia));
 			failureCount++;
 		}
@@ -37,9 +38,9 @@ public class TestAlacarte
 		
 		
 		int expectedPointsRemaining = initialPoints - testMedia.getPoints();
-		if (account1.getPointsRemaining() != expectedPointsRemaining))
+		if (account1.getPointsRemaining() != expectedPointsRemaining)
 		{
-			System.err.println("FAIL: Purchasing media incorrect deducts from points balance");
+			System.err.println("FAIL: Purchasing media incorrectly deducts from points balance");
 			System.err.println("Expected: " + expectedPointsRemaining);
 			System.err.println("Actual: " + account1.getPointsRemaining());
 			failureCount++;
@@ -47,6 +48,14 @@ public class TestAlacarte
 		
 		// Verify that the play method returns the correct message if there are insufficient points
 		String expected2 = "";
+		
+		if (!account1.play(testMedia).equals(expected2))
+		{
+			System.err.println("FAIL: Incorrect play message displayed if there are insufficient points");
+			System.err.println("Expected: " + expected2);
+			System.err.println("Actual: " + account1.play(testMedia));
+			failureCount++;
+		}
 		
 		// Exit program with failureCount or 0 depending on which data is used
 		if (failureCount > 0)
