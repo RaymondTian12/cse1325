0792ab3649adbb2f31bdc7f4a458e04e8fb5a120
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-16 14:42:47 -0500

    Added testing to confirm that the correct number of points are deducted in TestAlacarate.java

--- P04/test/TestAlacarte.java
+++ P04/test/TestAlacarte.java
@@ -7,7 +7,7 @@ public class TestAlacarte
 {
 	public static void main(String[] args)
 	{
-		Media testMedia = new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0", 15);
+		Media testMedia = new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0", 4);
 		int pointsBought = 5;
 		int failureCount = 0;
 		
@@ -23,8 +23,8 @@ public class TestAlacarte
 			failureCount++;
 		}
 		
-		// Veirfy that play returns "Playing " and the media if sufficient points are available 
-		String expected = "Playing " + testMedia.toString();
+		// Verify that play returns "Playing " and the media if sufficient points are available 
+		String expected1 = "Playing " + testMedia.toString();
 		if (!expected.equals(account1.play(testMedia)))
 		{
 			System.err.println("FAIL: Incorrect play message displayed");
@@ -33,6 +33,22 @@ public class TestAlacarte
 			failureCount++;
 		}
 		
+		// Verify that the play method reduces the points remaining by the number of points assigned if there are sufficient points
+		
+		
+		int expectedPointsRemaining = initialPoints - testMedia.getPoints();
+		if (account1.getPointsRemaining() != expectedPointsRemaining))
+		{
+			System.err.println("FAIL: Purchasing media incorrect deducts from points balance");
+			System.err.println("Expected: " + expectedPointsRemaining);
+			System.err.println("Actual: " + account1.getPointsRemaining());
+			failureCount++;
+		}
+		
+		// Verify that the play method returns the correct message if there are insufficient points
+		String expected2 = "";
+		
+		// Exit program with failureCount or 0 depending on which data is used
 		if (failureCount > 0)
 		{
 			System.err.println("FAIL Code: " + failureCount);
