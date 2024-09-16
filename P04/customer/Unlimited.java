64c9b3c19421561326fcd7ddbc88247bcec061a8
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-15 19:00:30 -0500

    Added JavaDoc for Unlimited.java

--- P04/customer/Unlimited.java
+++ P04/customer/Unlimited.java
@@ -1,9 +1,24 @@
+/**
+ * Represents an alacarte account type with no restrictions to playing media
+ *
+ * @author	Raymond Tian
+ * @version	1.0
+ * @since	1.0
+ */
+
 package customer;
 
 import product.Media;
 
 public class Unlimited extends Account
 {
+	/**
+	 * Returns a message displaying the media to be played with no restrictions
+	 *
+	 * @param media		the media to be played
+	 * @return		a string displaying the media that will be played
+	 * @since		1.0
+	 */
 	@Override
 	public String play(Media media)
 	{
