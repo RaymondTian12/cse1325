c6ad6ab1817d16f1f9787114e0235c69002929c9
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-27 18:20:06 -0500

    Added customer classes from P04

---
+++ P05/customer/Unlimited.java
@@ -0,0 +1,27 @@
+/**
+ * Represents an unlimited account type with no restrictions to playing media
+ *
+ * @author	Raymond Tian
+ * @version	1.0
+ * @since	1.0
+ */
+
+package customer;
+
+import product.Media;
+
+public class Unlimited extends Account
+{
+	/**
+	 * Returns a message displaying the media to be played with no restrictions
+	 *
+	 * @param media		the media to be played
+	 * @return		a string displaying the media that will be played
+	 * @since		1.0
+	 */
+	@Override
+	public String play(Media media)
+	{
+		return "Playing " + media.toString();
+	}
+}
