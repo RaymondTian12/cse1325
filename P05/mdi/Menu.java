960757a15d69f764f873e8518a8bc6c4ef3d37ea
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-27 18:12:27 -0500

    Created Menu.java

---
+++ P05/mdi/Menu.java
@@ -0,0 +1,24 @@
+package mdi;
+
+import java.util.ArrayList;
+
+public class Menu
+{
+	private ArrayList<MenuItems> items = new ArrayList<>();
+	
+	public void addMenuItem(MenuItem item)
+	{
+	
+	}
+	
+	@Override
+	public String toString()
+	{
+	
+	}
+	
+	public void run(int itemNumber)
+	{
+	
+	}
+}
