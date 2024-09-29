5f9692b43d29f4ed213c69a03785fb2fbe4fd217
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-29 17:56:23 -0500

    Started MenuItem.java class

---
+++ P05/mdi/MenuItem.java
@@ -0,0 +1,12 @@
+package mdi;
+
+public class MenuItem implements Runnable
+{
+	private Object menuText;
+	private Runnable menuResponse;
+	
+	public MenuItem(Object menuText, Runnable menuResponse)
+	{
+		
+	}
+}
