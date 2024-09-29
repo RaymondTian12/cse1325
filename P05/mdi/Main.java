d448f8778682269a77163f041141c6ee445156fd
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-29 18:13:56 -0500

    Started Main.java

---
+++ P05/mdi/Main.java
@@ -0,0 +1,26 @@
+package mdi;
+
+public class Main
+{
+	private Moes moes;
+	private String output;
+	private Menu menu;
+	private boolean running;
+	
+	private void addStudent()
+	{
+		
+	}
+	
+	private void listStudents()
+	{
+	
+	}
+	
+	
+	
+	public static void main(String[] args)
+	{
+	
+	}
+}
