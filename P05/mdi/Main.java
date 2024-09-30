ed615fe8d58dba5aae655f63843442a03a595101
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-30 15:46:40 -0500

    Added to addStudent() method

--- P05/mdi/Main.java
+++ P05/mdi/Main.java
@@ -1,5 +1,8 @@
 package mdi;
 
+import java.util.Scanner;
+import moes.Moes;
+
 public class Main
 {
 	private Moes moes;
@@ -9,6 +12,35 @@ public class Main
 	
 	private void addStudent()
 	{
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter Student Name: ");
+		String studentName = in.nextLine();
+		
+		System.out.println("Enter Student ID: ");
+		String studentID = in.nextLine();
+		
+		System.out.println("Enter Student Email: ");
+		String studentEmail = in.nextLine();
+		
+		System.out.println("Enter 1 for Alacarte or 2 for Unlimited: ");
+		int studentAccount = in.nextInt();
+		
+		if (studentAccount == 1)
+		{
+			Alacarte account = new Alacarte();
+		}
+		else if (studentAccount == 2)
+		{
+			Unlimited account = new Unlimited();
+		}
+		else
+		{
+			System.err.println("Not a valid account type!");
+			System.exit(-1);
+		}
+		
+		Student 
 		
 	}
 	
