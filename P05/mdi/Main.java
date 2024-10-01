479a40f5846f7c5878c0c7e7582ad650219effa1
Raymond Tian <rxt9181@mavs.uta.edu>
2024-09-30 20:17:47 -0500

    Saving progress

--- P05/mdi/Main.java
+++ P05/mdi/Main.java
@@ -14,25 +14,29 @@ public class Main
 	{
 		Scanner in = new Scanner(System.in);
 		
+		// Note: Student object - name is a string, id is int, email is string, and unlimited is boolean
 		System.out.println("Enter Student Name: ");
 		String studentName = in.nextLine();
 		
 		System.out.println("Enter Student ID: ");
-		String studentID = in.nextLine();
+		int studentID = in.nextInt();
+		in.nextLine(); // Consume the newline
 		
 		System.out.println("Enter Student Email: ");
 		String studentEmail = in.nextLine();
 		
 		System.out.println("Enter 1 for Alacarte or 2 for Unlimited: ");
 		int studentAccount = in.nextInt();
+		in.nextLine(); // Consume the newline
 		
+		boolean unlimitedAccount = false;
 		if (studentAccount == 1)
 		{
-			Alacarte account = new Alacarte();
+			unlimitedAccount = false;
 		}
 		else if (studentAccount == 2)
 		{
-			Unlimited account = new Unlimited();
+			unlimitedAccount = true;
 		}
 		else
 		{
@@ -40,16 +44,53 @@ public class Main
 			System.exit(-1);
 		}
 		
-		Student 
+		try
+		{
+			Student student = new Student(studentName, studentID, studentEmail, unlimitedAccount);
+		}
+		catch (IllegalArgumentException e)
+		{
+			System.err.println(e.getMessage());
+		}
 		
+		moes.addStudent(student);
 	}
 	
 	private void listStudents()
 	{
-	
+		System.out.println(moes.getStudentList());
 	}
 	
+	private void addMedia()
+	{
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter Media Title: ");
+		String mediaTitle = in.nextLine();
+		
+		System.out.println("Enter Media URL: ");
+		String mediaURL = in.nextLine();
+		
+		System.out.println("Enter Media Points Required: ");
+		int mediaPoints = in.nextInt();
+		in.nextLine(); // Consume the newline
+		
+		try 
+		{
+			Media media = new Media(mediaTitle, mediaUrl, mediaPoints);
+		} 
+		catch (IllegalArgumentException e) 
+		{
+			System.err.println("Failed to add media: " + e.getMessage());
+		}
+		
+		moes.addMedia(media);
+	}
 	
+	private void listMedia()
+	{
+		
+	}
 	
 	public static void main(String[] args)
 	{
