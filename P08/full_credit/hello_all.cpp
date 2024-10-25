ce72d212a010c4422a621f5ca0bc1ad6c3ef0ae5
Raymond Tian <rxt9181@mavs.uta.edu>
2024-10-25 09:02:21 -0500

    Finished hello_all.cpp

--- P08/full_credit/hello_all.cpp
+++ P08/full_credit/hello_all.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 
+int main(int argc, char* argv[])
+{
+	std::cout << "What's your name? ";
+	std::string name;
+	std::getline(std::cin, name);
+	std::cout << "Hello, " << name << "!" << std::endl;
+	
+	return 0;
+}
+
 
