e1f5d0d4e437904041d9f02eef83668c242674a7
Raymond Tian <rxt9181@mavs.uta.edu>
2024-11-03 13:47:34 -0600

    Finished clock.h

--- P09/full_credit/clock.h
+++ P09/full_credit/clock.h
@@ -1,4 +1,18 @@
 #ifndef __CLOCK_H
 #define __CLOCK_H
 
+class clock
+{
+	protected:
+		int _hours;
+		int _minutes;
+		int _seconds;
+	
+	public:
+		Clock(int hours, int minutes, int seconds);
+		virtual ~Clock();
+		void tic();
+		void print();
+};
 
+#endif
