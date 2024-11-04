be630b7db0a5bc9cda29031785d9f18a4dc03605
Raymond Tian <rxt9181@mavs.uta.edu>
2024-11-04 10:02:42 -0600

    Converted program arguments to ints

--- P09/full_credit/main.cpp
+++ P09/full_credit/main.cpp
@@ -3,5 +3,15 @@
 
 int main(int argc, char* argv[])
 {
-
+	if (argc != 4)
+	{
+		std::cerr << "usage: clock <hour> <minutes> <seconds>" << std::endl;
+		return -1;
+	}
+	
+	int hours = std::stoi(argv[1]);
+	int minutes = std::stoi(argv[2]);
+	int seconds = std::stoi(argv[3]);
+	
+	return 0;
 }
