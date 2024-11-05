3eb2a4f64d5f3efeee52313f4e4549bdd85cac8f
Raymond Tian <rxt9181@mavs.uta.edu>
2024-11-05 02:01:31 -0600

    Added timer.cpp

---
+++ P09/bonus/timer.cpp
@@ -0,0 +1,37 @@
+#include "timer.h"
+
+Timer::Timer(int hours, int minutes, int seconds)
+    : Clock(hours, minutes, seconds)
+{
+}
+
+Timer::~Timer()
+{
+}
+
+void Timer::tic()
+{
+	_seconds--;
+	if (_seconds == -1)
+	{
+		_seconds = 59;
+		_minutes--;
+		
+		if (_minutes == -1)
+		{
+			_minutes = 59;
+			_hours--;
+			
+			if (_hours == -1)
+			{
+				_seconds = 0;
+				_minutes = 0;
+				_hours = 0;
+				
+				throw std::runtime_error("Timer expired");
+			}
+		}
+	}
+}
+
+
