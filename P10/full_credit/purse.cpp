88a43c60bba47c0a34a441a76bc7aa1dea147bb3
Raymond Tian <rxt9181@mavs.uta.edu>
2024-11-10 01:15:53 -0600

    Finished remaining methods

--- P10/full_credit/purse.cpp
+++ P10/full_credit/purse.cpp
@@ -5,3 +5,68 @@ Purse::Purse(int pounds, int shillings, int pence)
 {
 	rationalize();
 }
+
+std::ostream& operator(std::ostream& ost, const Purse& purse)
+{
+	// Format: £3 4s5d - 3 pounds, 4 is shillings, and 5 is pence 
+	ost << "£" << purse._pounds << " " << purse._shillings << "s" << purse._pence << "d";
+	return ost;
+}
+
+Purse& Purse::operator++()
+{
+	_pence++;
+	rationalize();
+	return *this;
+}
+
+Purse Purse::operator++(int)
+{
+	Purse purse{*purse};
+	++*this;
+	return purse;
+}
+
+Purse Purse::operator+(const Purse& purse)
+{
+	return Purse(_pounds + purse._pounds, _shillings + purse._shillings, _pence + purse._pence);
+}
+
+Purse Purse::operator-(const Purse& purse)
+{
+	return Purse(_pounds - purse._pounds, _shillings - purse._shillings, _pence - purse._pence);
+}
+
+Purse& operator+=(const Purse& purse)
+{
+	_pounds += other._pounds;
+	_shillings += other._shillings;
+	_pence += other._pence;
+	rationalize();
+	return *this;
+}
+
+Purse& Purse::operator-=(const Purse& purse) 
+{
+    _pounds -= purse._pounds;
+    _shillings -= purse._shillings;
+    _pence -= purse._pence;
+    rationalize();
+    return *this;
+}
+
+void Purse::rationalize() 
+{
+	if (_pence >= 20)
+	{
+		_pence -= 20;
+		_shillings++;
+	}
+	
+	if (_shillings >= 12)
+	{
+		_shillings -= 12;
+		_pounds++;
+	}
+}
+
