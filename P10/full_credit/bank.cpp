200b7c1b353052ef093c81c5989731f160a46569
Raymond Tian <rxt9181@mavs.uta.edu>
2024-11-10 01:58:51 -0600

    Finished bank.cpp

---
+++ P10/full_credit/bank.cpp
@@ -0,0 +1,40 @@
+#include <map>
+#include "purse.h"
+
+int main(int argc, char* argv[])
+{
+	std::map<std::string, Purse> vault;
+	int numberOfAccounts;
+	
+	std::cout << "How many accounts would you like to create?";
+	std::cin >> numberOfAccounts;
+	std::cin.ignore();
+	
+	for (int i = 0; i < numberOfAccounts; i++)
+	{
+		std::string accountName;
+		int pounds, shillings, pence;
+		
+		std::cout << "Enter the name of the account: ";
+       		std::getline(std::cin, accountName);
+       		
+       		std::cout << "Enter initial deposit (pounds shillings pence): ";
+       		std::cin >> pounds >> shillings >> pence;
+		std::cin.ignore();
+		
+		vault[accountName] = Purse(pounds, shillings, pence);
+	}	
+	
+	Purse total; // Default Purse object
+	std::cout << "\nAccount List:\n";
+	
+	for (const auto& [name, purse] : vault) 
+	{
+        	std::cout << name << ": " << purse << "\n";
+        	total += purse;
+  	}
+  	
+  	std::cout << "Total amount in bank: " << total << "\n";
+	
+	return 0;
+}
