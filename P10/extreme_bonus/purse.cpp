0f0a203ee3eb06b50473714197012590d536f6f8
Raymond Tian <rxt9181@mavs.uta.edu>
2024-11-11 20:43:17 -0600

    Initialized static field

--- P10/extreme_bonus/purse.cpp
+++ P10/extreme_bonus/purse.cpp
@@ -1,5 +1,7 @@
 #include "purse.h"
 
+const std::string Purse::pound_utf8 = "\u00A3"; // In Unicode U+00A3 
+
 Purse::Purse(int pounds, int shillings, int pence)
 	: _pounds{pounds}, _shillings{shillings}, _pence{pence}
 {
