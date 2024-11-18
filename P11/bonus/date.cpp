29a6da0a7579d1af56b437176dfc7b506c324781
Raymond Tian <rxt9181@mavs.uta.edu>
2024-11-18 15:20:42 -0600

    Copied full credit solution

---
+++ P11/bonus/date.cpp
@@ -0,0 +1,20 @@
+#include "date.h"
+#include <iomanip>
+
+Date::Date(int year, int month, int day)
+	: _year{year}, _month{month}, _day{day}
+{
+} // No data validation required
+
+std::ostream& operator<<(std::ostream& ost, const Date& date)
+{
+	char old_setfill = ost.fill();
+	
+	ost << std::setw(4) << date._year << "/"
+            << std::setfill('0') << std::setw(2) << date._month << "/"
+            << std::setw(2) << date._day;
+            
+        std::setfill(old_setfill);
+        
+        return ost;
+}
---
+++ P11/bonus/list_temps.cpp
@@ -0,0 +1,94 @@
+#include "date.h"
+#include <iomanip>
+#include <map>
+#include <sstream>
+#include <fstream>
+
+typedef double Temp; 
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		std::cerr << "usage: <program name> <data file>\n";
+		return -1; 
+	}
+	
+	std::string filename = argv[1]; // Convert argument into filename string
+	std::ifstream ist{filename};
+	if (!ist)
+	{
+		std::cerr << "Can't open input file " << filename << "\n";
+		return -2;
+	}
+	
+	std::map<Date, Temp> temps;
+	std::string line;
+	
+	// Line Format: continent, country, state, region, month, day, year, and temperature in °F
+	// Ignore the first 4 fields 
+	while (std::getline(ist, line))
+	{
+		std::istringstream iss{line};
+		std::string continent, country, state, region; 
+		int month, day, year;
+		Temp temperature;
+		
+		// Ignore the first 4 fields
+		std::getline(iss, continent, ',');
+		std::getline(iss, country, ',');
+		std::getline(iss, state, ',');
+		std::getline(iss, region, ',');
+		
+		iss >> month;
+		iss.ignore(1, ',');  
+		iss >> day;
+		iss.ignore(1, ','); 
+		iss >> year;
+		iss.ignore(1, ',');
+		iss >> temperature;
+		
+		Date date(year, month, day);
+		temps[date] = temperature;
+	}
+	
+	while (true)
+	{
+		int startYear, startMonth, startDay;
+        	int endYear, endMonth, endDay;
+        	
+        	std::cout << "Enter the starting date to list (year month day): ";
+        	std::cin >> startYear >> startMonth >> startDay;
+        	
+        	if (!std::cin) 
+        	{
+       		 	break; 
+   		}
+
+        	
+        	std::cout << "Enter the ending date to list (year month day): ";
+        	std::cin >> endYear >> endMonth >> endDay;
+        	
+        	if (!std::cin) 
+        	{
+       		 	break; 
+   		}
+        	
+        	Date start(startYear, startMonth, startDay);
+        	Date end(endYear, endMonth, endDay);
+        	
+        	std::map<Date, Temp>::iterator it = temps.begin();
+        	while (it != temps.end())
+        	{
+        		Date date = it -> first;
+        		Temp temp = it -> second;
+        		
+        		if (date >= start && date <= end)
+        		{
+        			std::cout << std::setw(10) << date << "\t";
+               			std::cout << std::fixed << std::setprecision(1) << temp << "\n";
+        		}
+        		it++;
+        	}
+	}
+}
