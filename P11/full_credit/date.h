8a3fdf260343eae182714fe67b747159e6d26a2f
Raymond Tian <rxt9181@mavs.uta.edu>
2024-11-18 11:51:41 -0600

    Finished date.h implementation

--- P11/full_credit/date.h
+++ P11/full_credit/date.h
@@ -5,7 +5,15 @@
 
 class Date
 {
-
+	private:
+		int _year;
+		int _month;
+		int _day;
+	
+	public: 
+		Date(int year = 1970, int month = 1, int day = 1); // January 1, 1970
+		auto operator<=>(const Date&) const = default;
+		friend std::ostream& operator<<(std::ostream& ost, const Date& date);
 };
 
 #endif
