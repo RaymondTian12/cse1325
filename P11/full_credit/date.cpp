e09a6d6be5d24ca2751e2746b57bff24caa12e5e
Raymond Tian <rxt9181@mavs.uta.edu>
2024-11-18 12:06:38 -0600

    Implemented operator<<

--- P11/full_credit/date.cpp
+++ P11/full_credit/date.cpp
@@ -5,4 +5,15 @@ Date::Date(int year, int month, int day)
 {
 } // No data validation required
 
-
+std::ostream& operator<<(std::ostream& ost, const Date& date)
+{
+	char old_setfill = ost.fill();
+	
+	ost << std::setw(4) << date._year << "/"
+            << std::setfill('0') << std::setw(2) << date._month << "/"
+            << std::setw(2) << date._day;
+            
+        std::setfill(old_setfill);
+        
+        return ost;
+}
