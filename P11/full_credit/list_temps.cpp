ba6f4e80177ff0794dd8e84b5a8f8f4ce93fb584
Raymond Tian <rxt9181@mavs.uta.edu>
2024-11-18 12:22:17 -0600

    Added typedef, argument verification, and started file opening

--- P11/full_credit/list_temps.cpp
+++ P11/full_credit/list_temps.cpp
@@ -1,6 +1,16 @@
 #include "date.h"
 
+typedef double Temp; 
+
 int main(int argc, char* argv[])
 {
-
+	if (argc != 2)
+	{
+		std::cerr << "usage: <program name> <data file>\n";
+		return -1; 
+	}
+	
+	std::string filename = argv[1]; // Convert argument into filename string
+	std::ifstream ist{filename};
+	 
 }
