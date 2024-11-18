22c92a4271035c8fc8a3573fba51480df351145a
Raymond Tian <rxt9181@mavs.uta.edu>
2024-11-18 13:52:26 -0600

    Created map and started file I/O

--- P11/full_credit/list_temps.cpp
+++ P11/full_credit/list_temps.cpp
@@ -12,5 +12,24 @@ int main(int argc, char* argv[])
 	
 	std::string filename = argv[1]; // Convert argument into filename string
 	std::ifstream ist{filename};
-	 
+	if (!ist)
+	{
+		std::cerr << "Can't open input file " << filename << "\n";
+		return -2;
+	}
+	
+	std::map<Date, Temp> temps;
+	std::string line;
+	
+	// Line Format: continent, country, state, region, month, day, year, and temperature in °F
+	// Ignore the first 4 fields 
+	while (std::getline(ist, line))
+	{
+		std::istringstream stream{line};
+		std::string continuent, country, state, region; 
+		int month, day, year;
+		Temp temperature;
+		
+		std::getline(stream, string, ' ,');
+	}
 }
